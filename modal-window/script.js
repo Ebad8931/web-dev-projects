3fcbb38aac23d56b82b0a9d4b57634b1c7b23f4f
Ebad Qureshi <ebad.qureshi922@gmail.com>
2026-04-21 17:02:32 -0500

    open modal functionality added

--- modal-window/script.js
+++ modal-window/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const openModalButtons = document.querySelectorAll(".show-modal");
+
+const overlay = document.querySelector(".overlay");
+const modalWindow = document.querySelector(".modal");
+
+openModalButtons.forEach(modalButton => {
+    modalButton.addEventListener('click', () => {
+        modalWindow.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+});
