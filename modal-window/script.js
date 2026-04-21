3abf26f00f5f40ff746380509e403385b18292b9
Ebad Qureshi <ebad.qureshi922@gmail.com>
2026-04-21 17:09:04 -0500

    close modal functionality added

--- modal-window/script.js
+++ modal-window/script.js
@@ -1,13 +1,27 @@
 'use strict';
 
-const openModalButtons = document.querySelectorAll(".show-modal");
+const modalOpenButtons = document.querySelectorAll(".show-modal");
 
 const overlay = document.querySelector(".overlay");
 const modalWindow = document.querySelector(".modal");
 
-openModalButtons.forEach(modalButton => {
-    modalButton.addEventListener('click', () => {
-        modalWindow.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
+const modalCloseButton = document.querySelector(".close-modal");
+
+// helper functions to open and close modal window
+function openModalWindow() {
+    modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+function closeModalWindow() {
+    modalWindow.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+modalOpenButtons.forEach(modalButton => {
+    modalButton.addEventListener('click', openModalWindow);
 });
+
+modalCloseButton.addEventListener('click', closeModalWindow);
+
+overlay.addEventListener('click', closeModalWindow);
