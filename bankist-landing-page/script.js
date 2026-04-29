cc3f8274a801e97bcabf31c0e678425d1dd68884
Ebad Qureshi <ebad.qureshi922@gmail.com>
2026-04-28 23:08:20 -0500

    basic slider using images

--- bankist-landing-page/script.js
+++ bankist-landing-page/script.js
@@ -49,3 +49,49 @@ btnToScroll.addEventListener('click', () => {
     const section1 = document.querySelector('#section--1')
     section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+// slider
+const slider = document.querySelector('.slider');
+
+// slider.style.transform = 'scale(0.4) translateX(-800px)';
+// slider.style.overflow = 'visible';
+
+const slides = document.querySelectorAll('.slide');
+
+let currSlide = 0;
+
+slides.forEach((s, i) => {
+    s.style.transform = `translateX(${i * 100}%)`;
+});
+
+const btnRight = document.querySelector('.slider__btn--right');
+const btnLeft = document.querySelector('.slider__btn--left');
+
+btnRight.addEventListener('click', () => {
+    if (currSlide === slides.length - 1) {
+        // on last slide, so go back to first
+        currSlide = 0;
+    } else {
+        currSlide++;
+    }
+    console.log(currSlide);
+    slides.forEach((s, i) => {
+        console.log(s, i, currSlide);
+        s.style.transform = `translateX(${(i - currSlide) * 100}%)`;
+        console.log(s, i, currSlide);
+    });
+});
+
+btnLeft.addEventListener('click', () => {
+    if (currSlide === 0) {
+        // on first slide, so go back to first
+        currSlide = slides.length - 1;
+    } else {
+        currSlide--;
+    }
+
+    slides.forEach((s, i) => {
+        s.style.transform = `translateX(${(i - currSlide) * 100}%)`;
+    });
+
+});
